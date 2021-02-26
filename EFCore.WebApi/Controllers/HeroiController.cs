2699e5342df7dc2943a4ca128d5bc6f4f1dd66c3
IgorSouzaBezerra <igor.souza1.bezerra@gmail.com>
2021-02-26 14:10:34 -0300

    initial tests in controller heroi

---
+++ EFCore.WebApi/Controllers/HeroiController.cs
@@ -0,0 +1,35 @@
+﻿using EFCore.Domain;
+using EFCore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HeroiController : ControllerBase
+    {
+        public readonly HeroiContext _context;
+
+        public HeroiController(HeroiContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpPost]
+        public IActionResult Post()
+        {
+            var heroi = new Heroi { Nome = "Homem de Ferro" };
+            _context.Herois.Add(heroi);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var herois = _context.Herois;
+            return Ok(herois);
+        }
+    }
+}
