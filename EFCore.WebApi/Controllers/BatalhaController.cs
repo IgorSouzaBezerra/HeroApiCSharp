f9d9d5bc29b8b1ce9304e55c25e15f22b07982e3
IgorSouzaBezerra <igor.souza1.bezerra@gmail.com>
2021-02-28 00:02:00 -0300

    Add controller Batalha

---
+++ EFCore.WebApi/Controllers/BatalhaController.cs
@@ -0,0 +1,47 @@
+﻿using EFCore.Domain;
+using EFCore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace EFCore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BatalhaController : ControllerBase
+    {
+        private readonly HeroiContext _context;
+        public BatalhaController(HeroiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(new Batalha());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex}");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Batalha model)
+        {
+            try
+            {
+                _context.Batalhas.Add(model);
+                _context.SaveChanges();
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex}");
+            }
+        }
+    }
+}
